076b0cca6e6c75ab8e46c27703b918833e930643
Farben <farbeninteractive@gmail.com>
2019-01-22 02:07:40 -0600

    Contenido Dinámico Home y Media

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,12 +6,20 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'home.html',
 })
 export class HomePage {
+    private _dataCards: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+    constructor(public navCtrl: NavController, public navParams: NavParams) {
+        this._dataCards = [
+            { title: "Enjoying a Coffee", image: "adult-attractive.png",         descripcion: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod. Tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam." },
+            { title: "Sharing Moments",   image: "aroma-art-beverage.png",       descripcion: "Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo. Cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non." },
+            { title: "Happiness",         image: "art-blur-cappuccino.png",      descripcion: "Consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse. Tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam." },
+            { title: "Tasting a Coffee",  image: "attractive-bar-barista.png",   descripcion: "Cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non. Tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam." },
+            { title: "Coffe & Work",      image: "beverage-coffee-computer.png", descripcion: "Proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non." }
+        ];
+    }
 
-  ionViewDidLoad() {
+    ionViewDidLoad() {
     console.log('ionViewDidLoad HomePage');
-  }
+    }
 
 }
--- src/pages/media/media.ts
+++ src/pages/media/media.ts
@@ -6,8 +6,16 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'media.html',
 })
 export class MediaPage {
+    private _dataMedia: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    constructor(public navCtrl: NavController, public navParams: NavParams) {
+        this._dataMedia = [
+            { title: "Coffee",         video: "CoffeeBeans.mp4",    poster: "CoffeeBeans-poster.png",    descripcion: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod. Tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam." },
+            { title: "Coffe & Work",   video: "CoffeeComputer.mp4", poster: "CoffeeComputer-poster.png", descripcion: "Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo. Cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non." },
+            { title: "Steaming",       video: "CoffeeCoup.mp4",     poster: "CoffeeCoup-poster.png",     descripcion: "Consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse. Tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam." },
+            { title: "Latte",          video: "CoffeeMachine.mp4",  poster: "CoffeeMachine-poster.png",  descripcion: "Cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non. Tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam." },
+            { title: "American",       video: "CoffeeMorning.mp4",  poster: "CoffeeMorning-poster.png",  descripcion: "Proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non." }
+        ];
   }
 
   ionViewDidLoad() {
